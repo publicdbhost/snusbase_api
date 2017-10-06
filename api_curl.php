7ed76be9bb999a351590be0d5875d8fafb657f65
publicdbhost <jgt@cock.li>
2017-10-06 21:50:12 +0200

    Create api_curl.php
    wildcard and limit are optional, leave blank if you're not using them. Alternatively wildcard can be toggled with false/true and limit can be set to any number you want

---
+++ api_curl.php
@@ -0,0 +1,27 @@
+<?php
+function apiRequest(array $postData) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://yourapi.example.com"); // Replace with your endpoint
+    curl_setopt($ch, CURLOPT_POST, true);
+	  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: YourTokenHere')); // Replace this with your auth token
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+    curl_setopt($ch, CURLOPT_TIMEOUT, 40); // may want to reduce this
+    curl_setopt($ch, CURLOPT_USERAGENT, "Snusbase");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    $curl_info = curl_getinfo($ch);
+
+    return compact('response', 'curl_info');
+    if(curl_errno($ch))
+    {
+        echo 'Curl error: ' . curl_error($ch);
+    }
+}
+
+// Creates array from post form
+$postArray = Array("type" => $post_type, "term" => $post_term, "wildcard" => $wildcard, "limit" => $limit);
+
+// Passes array to function
+$response = apiRequest($postArray);
+?>
