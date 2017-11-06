72696e48f83eac21fd9e83747d66bd91e97dea05
publicdbhost <jgt@cock.li>
2017-11-06 03:01:08 +0100

    Update api_curl.php

--- api_curl.php
+++ api_curl.php
@@ -1,11 +1,17 @@
 <?php
+// Official Snusbase sample code. This code is provided as-is and is only for refference of how to
+// properly handle our API. For API access contact us through one of our support channels, and we
+// can make a deal depending on your usage case. API packages start at $80/month.
+
+
+
 function apiRequest(array $postData) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "https://yourapi.example.com"); // Replace with your endpoint
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: YourTokenHere')); // Replace this with your auth token
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
-    curl_setopt($ch, CURLOPT_TIMEOUT, 40); // may want to reduce this
+    curl_setopt($ch, CURLOPT_TIMEOUT, 40);
     curl_setopt($ch, CURLOPT_USERAGENT, "Snusbase");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
